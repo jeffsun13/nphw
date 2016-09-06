a6bdc4ff599ef89d7ee7f6f461f845f59cd7da69
VICTORIA JUAN <squintyblu@gmail.com>
2016-09-06 14:04:34 -0700

    Button goes to login screen 9/6

--- app/src/main/java/app/com/example/victoriajuan/nphshomework/HomeActivity.java
+++ app/src/main/java/app/com/example/victoriajuan/nphshomework/HomeActivity.java
@@ -1,16 +1,15 @@
 package app.com.example.victoriajuan.nphshomework;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -34,10 +33,9 @@ public class HomeActivity extends AppCompatActivity {
 
         button.setOnClickListener(
                 new Button.OnClickListener() {
+                    @Override
                     public void onClick(View v) {
-                        TextView myTextView =
-                                (TextView) findViewById(R.id.myTextView);
-                        myTextView.setText("Button clicked");
+                        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
                     }
                 });
     }
