820a7279e4a1001e592f99202af3ce51da174d4d
vickyjjj <squintyblu@gmail.com>
2016-09-08 14:50:07 -0700

    Added ListView for Classes 9/8

--- app/src/main/java/app/com/example/victoriajuan/nphshomework/ClassActivity.java
+++ app/src/main/java/app/com/example/victoriajuan/nphshomework/ClassActivity.java
@@ -1,30 +1,114 @@
 package app.com.example.victoriajuan.nphshomework;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
 public class ClassActivity extends AppCompatActivity {
 
+    public ClassActivity() {
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if(savedInstanceState==null)
+            getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new PlaceholderFragment())
+                .commit();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_class, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return onOptionsItemSelected(item);
+    }
+
+    public static class PlaceholderFragment extends Fragment{
+        private ArrayAdapter<String> mForecastAdapter;
+
+        public PlaceholderFragment(){
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            String[] data= {
+                    "English: Read pages",
+                    "Math: Do Problems",
+                    "History: Watch Video",
+                    "Science: Do Lab",
+                    "Elective: Take Pictures",
+                    "Elective2: Nothing"
+            };
+
+            List<String> weekHomework=new ArrayList<String>(Arrays.asList(data));
+
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekHomework);
+
+            View rootView=inflater.inflate(R.layout.content_class, container, false);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_classes);
+            listView.setAdapter(mForecastAdapter);
+
+            return rootView;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 
 
     }
-}
