62344568572fd73b78fa3d5ac7a3681b5a2a0457
VICTORIA JUAN <squintyblu@gmail.com>
2016-09-17 17:57:59 -0700

    Added communication between savesharedpreferences and settings activity

--- app/src/main/java/app/com/example/victoriajuan/nphshomework/SaveSharedPreference.java
+++ app/src/main/java/app/com/example/victoriajuan/nphshomework/SaveSharedPreference.java
@@ -3,19 +3,14 @@ package app.com.example.victoriajuan.nphshomework;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.view.View;
-
-import java.util.Calendar;
 
 public class SaveSharedPreference {
     static final String PREF_USER_NAME = "username";
-    static final String DAY = "balls";
+    static final String DAY = "balls"; //ok jeff
     static final String MONTH = "ball";
     static final String YEAR = "nuts";
-
-
-
-
+    static final String NIGHT_NOT = "";
+    static final String MORN_NOT = "";
 
     static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
@@ -48,4 +43,16 @@ public class SaveSharedPreference {
     public static int getYear(Context ctx) {
         return getSharedPreferences(ctx).getInt(YEAR, 0);
     }
+
+    public static void setNotifications(Context ctx, String str, String val) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        if (str.equals("pref_night_notif_key"))
+            editor.putBoolean(NIGHT_NOT, Boolean.parseBoolean(val));
+        else
+            editor.putBoolean(MORN_NOT, Boolean.parseBoolean(val));
+        editor.apply();
+    }
+
+    public static boolean getMornNot(Context ctx) { return getSharedPreferences(ctx).getBoolean(MORN_NOT, true); }
+    public static boolean getNightNot(Context ctx) { return getSharedPreferences(ctx).getBoolean(NIGHT_NOT, true); }
 }
--- app/src/main/java/app/com/example/victoriajuan/nphshomework/SettingsActivity.java
+++ app/src/main/java/app/com/example/victoriajuan/nphshomework/SettingsActivity.java
@@ -29,6 +29,8 @@ public class SettingsActivity extends PreferenceActivity
     @Override
     public boolean onPreferenceChange(Preference preference, Object value) {
 
+        SaveSharedPreference.setNotifications(SettingsActivity.this, preference.getKey(), value.toString());
+
         return true;
     }
 
